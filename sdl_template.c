e380d96de337a065a2ae761dd8385407d38813d8
Wally Young <wally.wy.young@gmail.com>
2016-02-26 11:10:31 -0800

    Updating CMake file, drawing triangle.

--- sdl_template.c
+++ sdl_template.c
@@ -1,26 +1,42 @@
 #define GLEW_STATIC
 #include<GL/glew.h>
-
 #include<SDL2/SDL.h>
 #include<SDL2/SDL_opengl.h>
 #undef main
 
 void createWindow(SDL_Window **window);
 void createContext(SDL_Window *window, SDL_GLContext *context);
+void draw();
 int  shutDown(SDL_GLContext* context);
-//char *vertShader = "#version 150\nin vec2 position;\nvoid main() {\ngl_Position = vec4(position, 0.0, 1.0);\n}";
-//char *fragShader = "#version 150\nout vec4 outColor;\nvoid main() {\noutColor = vec4(1.0, 1.0, 1.0, 1.0);\n}"
-const char *fragShader = SIMPLE_FRAG;
-const char *vertShader = SIMPLE_VERT;
+const char *vertShader = R"(
+#version 150
+
+in vec2 position;
+
+void main() {
+    gl_Position = vec4(position, 0.0, 1.0);
+}
+)";
+const char *fragShader = R"(
+#version 150
+
+out vec4 outColor;
+
+void main() {
+    outColor = vec4(1.0, 1.0, 1.0, 1.0);
+}
+)";
 
 int main(int argc, char *argv[]) {
     SDL_Window    *window = NULL;
     SDL_GLContext context = NULL;
+
     createWindow(&window);
     createContext(window, &context);
 
     SDL_Event windowEvent;
     for(;;) {
+        draw();
         if (SDL_PollEvent(&windowEvent)) {
             if (windowEvent.type == SDL_QUIT) {
                 break;
@@ -37,11 +53,13 @@ void createWindow(SDL_Window **window) {
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
     *window  = SDL_CreateWindow("OpenGL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
 }
+
 float vertices[] = {
-        0.0f,  0.5f, // Vertex 1 (X,Y)
-        0.5f, -0.5f, // Vertex 2 (X,Y)
+         0.0f,  0.5f, // Vertex 1 (X,Y)
+         0.5f, -0.5f, // Vertex 2 (X,Y)
         -0.5f,  0.5f  // Vertex 3 (X,Y)
 };
+
 void createContext(SDL_Window *window, SDL_GLContext *context) {
     *context = SDL_GL_CreateContext(window);
     glewExperimental = GL_TRUE;
@@ -67,7 +85,12 @@ void createContext(SDL_Window *window, SDL_GLContext *context) {
     glUseProgram(shaderProgram);
 
 
+    GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(posAttrib);
 
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
 }
 
 int shutDown(SDL_GLContext *context) {
@@ -77,5 +100,5 @@ int shutDown(SDL_GLContext *context) {
 }
 
 void draw() {
-
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 }
